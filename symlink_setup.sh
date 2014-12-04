4996c18d14c85261c120c0ca2bd7b2be47b2fd3b
Max Seiden <max@platfora.com>
2014-12-04 13:03:39 -0800

    more symlink updates - added some logging

--- symlink_setup.sh
+++ symlink_setup.sh
@@ -1,2 +1,10 @@
-ln -fs ./vim           $HOME/.vim
-ln -fs ./vimrc         $HOME/.vimrc
+echo Current directory is `pwd`
+REPO=`pwd`
+
+echo Linking $REPO/vim to $HOME/.vim
+rm -f $HOME/.vim
+ln -fs $REPO/vim $HOME/.vim
+
+echo Linking vimrc to $HOME/.vimrc
+rm -f $HOME/.vimrc
+ln -fs $REPO/vimrc $HOME/.vimrc
