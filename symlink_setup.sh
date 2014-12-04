0a11644aae897f7ddb99e9774410e6b16fd449d5
Max Seiden <max@platfora.com>
2014-12-04 12:59:04 -0800

    removing gitconfig since mine is different on various machines; update symlink setup to not assume a fs location

--- symlink_setup.sh
+++ symlink_setup.sh
@@ -1,3 +1,2 @@
-ln -fs ~/.dotfiles/vim           ~/.vim
-ln -fs ~/.dotfiles/vimrc         ~/.vimrc
-ln -fs ~/.dotfiles/gitconfig     ~/.gitconfig
+ln -fs ./vim           $HOME/.vim
+ln -fs ./vimrc         $HOME/.vimrc
